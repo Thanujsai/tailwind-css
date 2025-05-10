8695b2ed0c3963704c6eb8eb5fb25d3a810288c9
thanujsai <sboda@modeln.com>
2025-05-10 18:32:22 +0530

    new commit

---
+++ resources/views/components/plans.blade.php
@@ -0,0 +1,15 @@
+<section id="plans">
+    <div class="text-center py-10">
+        <h1 class="text-4xl font-bold text-center">Plans</h1>
+    </div>
+
+    <div class="flex max-w-6xl mx-auto mb-52 text-center">
+        <div class="flex flex-col items-center p-6 space-y-6 shadow-xl rounded-lg bg-gray-100 md:w-1/3">
+            <img src="img/avatar-anisha.png"  class="w-16 -mt-14"/>{{-- -mt-14: This negative margin-top pulls the image up, overlapping the top of the card. --}}
+            <h5 class="text-lg font-bold">Anisha Li</h5>
+            <p class="text-sm">
+                “Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.”
+            </p>
+        </div>
+    </div>
+</section>
